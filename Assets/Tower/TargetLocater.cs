622d6ecf0a7902453d953be17bb33dc92211cec6
berdozo <bbdurusan@gmail.com>
2024-09-04 21:33:58 +0300

    Targeting closest enemy.

--- Assets/Tower/TargetLocater.cs
+++ Assets/Tower/TargetLocater.cs
@@ -6,24 +6,59 @@ using UnityEngine;
 public class TargetLocater : MonoBehaviour
 {   
     [SerializeField] Transform weapon;
+    [SerializeField] float towerRange=15f;
+    [SerializeField] ParticleSystem projectileParticles;
     Transform target;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        target = FindObjectOfType<EnemyMover>().transform;
-    }
 
     
 
     // Update is called once per frame
     void Update()
     {
+        TargetClosestEnemy();
         AimWepon();
+    }
+
+
+    void TargetClosestEnemy()
+    {
+        Enemy[] enemies = FindObjectsOfType<Enemy>();
+        Transform closestEnemy = null;
+        float maxDistance = Mathf.Infinity;
+
+        foreach(Enemy enemy in enemies)
+        {
+            float targetDistance = Vector3.Distance(transform.position,enemy.transform.position);
+            
+            if(targetDistance<maxDistance)
+            {
+                closestEnemy = enemy.transform;
+                maxDistance = targetDistance;
+            }
+        }
+
+        target = closestEnemy.transform;
     }
 
     void AimWepon()
     {
+        float targetDistance = Vector3.Distance(transform.position,target.transform.position);
+        
         weapon.LookAt(target);
+
+        if(targetDistance<=towerRange)
+        {
+            Atack(true);
+        }
+        else
+        {
+            Atack(false);
+        }
+    }
+
+    void Atack(bool isActive)
+    {
+        var emmisionModule = projectileParticles.emission;
+        emmisionModule.enabled = isActive;
     }
 }
